d76dd487a47388947ad58281d878d40d5f1bf52f
Cole Folkers <118484968+ElocF@users.noreply.github.com>
2023-11-10 14:28:48 -0800

    Delete Todays_Date.sql

--- Todays_Date.sql
+++
@@ -1,7 +0,0 @@
--- Get todays date
-SELECT TO_CHAR(TRUNC(SYSDATE + 1) - 1/(24*60*60) ,'yyyy-Mon-dd ') FROM DUAL
-
---Or below can be used after select statment 
-
-where
-to_char (f.fmc_to_date, 'YYYY-Mon-DD') >= sysdate
